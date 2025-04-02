a8029e5aeac7be4332087c51886d224bb978278d
Jared <jrsb@google.com>
2025-04-02 12:02:49 -0700

    Bump to the next snapshot version (#127)

--- artifactregistry-gradle-plugin/src/main/java/com/google/cloud/artifactregistry/gradle/plugin/ArtifactRegistryGradlePlugin.java
+++ artifactregistry-gradle-plugin/src/main/java/com/google/cloud/artifactregistry/gradle/plugin/ArtifactRegistryGradlePlugin.java
@@ -21,11 +21,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.artifactregistry.auth.CommandExecutor;
 import com.google.cloud.artifactregistry.auth.CredentialProvider;
 import com.google.cloud.artifactregistry.auth.DefaultCredentialProvider;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.net.URI;
-import java.net.URISyntaxException;
-import javax.annotation.Nullable;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.ProjectConfigurationException;
@@ -35,7 +31,6 @@ import org.gradle.api.artifacts.repositories.PasswordCredentials;
 import org.gradle.api.credentials.Credentials;
 import org.gradle.api.initialization.Settings;
 import org.gradle.api.initialization.dsl.ScriptHandler;
-import org.gradle.api.initialization.resolve.DependencyResolutionManagement;
 import org.gradle.api.internal.artifacts.repositories.DefaultMavenArtifactRepository;
 import org.gradle.api.invocation.Gradle;
 import org.gradle.api.provider.Property;
@@ -43,176 +38,199 @@ import org.gradle.api.provider.ProviderFactory;
 import org.gradle.api.publish.PublishingExtension;
 import org.gradle.api.tasks.Input;
 import org.gradle.internal.authentication.DefaultBasicAuthentication;
-import org.gradle.plugin.management.PluginManagementSpec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.annotation.Nullable;
+
 public class ArtifactRegistryGradlePlugin implements Plugin<Object> {
 
-  private static final Logger logger = LoggerFactory.getLogger(ArtifactRegistryGradlePlugin.class);
+    private static final Logger logger = LoggerFactory.getLogger(ArtifactRegistryGradlePlugin.class);
 
-  static class ArtifactRegistryPasswordCredentials implements PasswordCredentials {
-    private String username;
-    private String password;
-    private final CommandExecutor commandExecutor;
+    static class ArtifactRegistryPasswordCredentials implements PasswordCredentials {
+        private String username;
+        private String password;
+        private final CommandExecutor commandExecutor;
 
-    ArtifactRegistryPasswordCredentials(String username, String password, CommandExecutor commandExecutor) {
-      this.username = username;
-      this.password = password;
-      this.commandExecutor = commandExecutor;
+        ArtifactRegistryPasswordCredentials(String username, String password, CommandExecutor commandExecutor) {
+            this.username = username;
+            this.password = password;
+            this.commandExecutor = commandExecutor;
+        }
+
+        @Input
+        @Override
+        public String getUsername() {
+            return username;
+        }
+
+        @Input
+        @Override
+        public String getPassword() {
+            return password;
+        }
+
+        @Override
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        @Override
+        public void setPassword(String password) {
+            this.password = password;
+        }
     }
 
-    @Input
-    @Override
-    public String getUsername() {
-      return username;
-    }
-
-    @Input
-    @Override
-    public String getPassword() {
-      return password;
-    }
-
-    @Override
-    public void setUsername(String username) {
-      this.username = username;
-    }
-
-    @Override
-    public void setPassword(String password) {
-      this.password = password;
-    }
-  }
-
-  private final CredentialProvider credentialProvider = DefaultCredentialProvider.getInstance();
+    private final CredentialProvider credentialProvider = DefaultCredentialProvider.getInstance();
+    private ArtifactRegistryPasswordCredentials crd = null;
 
   @Override
   public void apply(Object o) {
-    ArtifactRegistryPasswordCredentials crd = null;
+    logger.error("Using my local plugin");
     try {
       ProviderFactory providerFactory;
       if (o instanceof Project) {
+        logger.error("Applying to project");
         providerFactory = ((Project) o).getProviders();
+        crd = getArtifactRegistryPasswordCredentials(providerFactory, crd);
+        applyProject((Project) o, crd);
       } else if (o instanceof Gradle) {
-        providerFactory = ((Gradle) o).getRootProject().getProviders();
-      } else if (o instanceof Settings) {
-        providerFactory = ((Settings) o).getProviders();
-      } else {
-        logger.info("Failed to get access token from gcloud or Application Default Credentials due to unknown script type " + o);
-        return;
-      }
-      CommandExecutor commandExecutor = new ProviderFactoryCommandExecutor(providerFactory);
+        Gradle grdl = (Gradle) o;
+        try {
+          providerFactory = grdl.getRootProject().getProviders();
+          crd = getArtifactRegistryPasswordCredentials(providerFactory, crd);
 
-      GoogleCredentials credentials = (GoogleCredentials)credentialProvider.getCredential(commandExecutor);
-      credentials.refreshIfExpired();
-      AccessToken accessToken = credentials.getAccessToken();
-      String token = accessToken.getTokenValue();
-      crd = new ArtifactRegistryPasswordCredentials("oauth2accesstoken", token, commandExecutor);
+          logger.error("Applying to gradle");
+          applyGradle(grdl, crd);
+        } catch (IllegalStateException e) {
+          logger.error("Applying to settings during init: ");
+          grdl.beforeSettings(settings -> {
+            logger.error("Applying now to " + settings.getSettingsDir().toString());
+            settings.getPlugins().apply(ArtifactRegistryGradlePlugin.class);
+
+          });
+        }
+      } else if (o instanceof Settings) {
+        logger.error("Applying to settings");
+        providerFactory = ((Settings) o).getProviders();
+        crd = getArtifactRegistryPasswordCredentials(providerFactory, crd);
+        applySettings((Settings) o, crd);
+      }
+
     } catch (IOException e) {
       logger.info("Failed to get access token from gcloud or Application Default Credentials", e);
-    }
-
-    if (o instanceof Project) {
-      applyProject((Project) o, crd);
-    } else if (o instanceof Gradle) {
-      applyGradle((Gradle) o, crd);
-    } else if (o instanceof Settings) {
-      applySettings((Settings) o, crd);
-    }
-  }
-
-  // The plugin for Gradle will apply Artifact Registry repo settings inside settings.gradle and build.gradle.
-  private void applyGradle(Gradle gradle, @Nullable ArtifactRegistryPasswordCredentials crd) {
-    gradle.settingsEvaluated(s -> modifySettings(s, crd));
-    gradle.projectsLoaded(g -> g.allprojects(p -> modifyProjectBuildscript(p, crd)));
-    gradle.projectsEvaluated(g -> g.allprojects(p -> modifyProject(p, crd)));
-  }
-
-  // The plugin for settings will apply Artifact Registry repo settings inside settings.gradle and build.gradle.
-  private void applySettings(Settings settings, @Nullable ArtifactRegistryPasswordCredentials crd) {
-    applyGradle(settings.getGradle(), crd);
-  }
-
-  // The plugin for projects will only apply Artifact Registry repo settings inside build.gradle.
-  private void applyProject(Project project, @Nullable ArtifactRegistryPasswordCredentials crd) {
-    project.afterEvaluate(p -> modifyProject(p, crd));
-  }
-
-  private void modifyProject(Project p, @Nullable ArtifactRegistryPasswordCredentials crd) {
-    p.getRepositories().forEach(r -> configureArtifactRegistryRepository(r, crd));
-    final PublishingExtension publishingExtension = p.getExtensions().findByType(PublishingExtension.class);
-    if (publishingExtension != null) {
-      publishingExtension.getRepositories().forEach(r -> configureArtifactRegistryRepository(r, crd));
-    }
-  }
-
-  // Not sure this knows which repositories already exist, so register a callback to modify the repos
-  // as they are added to the buildscript
-  private void modifyProjectBuildscript(Project p, @Nullable ArtifactRegistryPasswordCredentials crd) {
-    final ScriptHandler buildscript = p.getBuildscript();
-    if (buildscript != null) {
-      buildscript.getRepositories().whenObjectAdded(r -> configureArtifactRegistryRepository(r, crd));
-    }
-  }
-
-  private void modifySettings(Settings s, @Nullable ArtifactRegistryPasswordCredentials crd) {
-    s.getPluginManagement().getRepositories().forEach(r -> configureArtifactRegistryRepository(r, crd));
-    s.getDependencyResolutionManagement().getRepositories().forEach(r -> configureArtifactRegistryRepository(r, crd));
-  }
-
-  private void configureArtifactRegistryRepository(
-      ArtifactRepository repo, @Nullable ArtifactRegistryPasswordCredentials crd)
-      throws ProjectConfigurationException, UncheckedIOException {
-    if (!(repo instanceof DefaultMavenArtifactRepository)) {
       return;
     }
-    final DefaultMavenArtifactRepository arRepo = (DefaultMavenArtifactRepository) repo;
-    final URI u = arRepo.getUrl();
-    if (u != null && u.getScheme() != null && u.getScheme().equals("artifactregistry")) {
-      try {
-        arRepo.setUrl(new URI("https", u.getHost(), u.getPath(), u.getFragment()));
-      } catch (URISyntaxException e) {
-        throw new ProjectConfigurationException(
-            String.format("Invalid repository URL %s", u.toString()), e);
-      }
-
-      if (crd != null && shouldStoreCredentials(arRepo)) {
-        arRepo.setConfiguredCredentials(crd);
-        arRepo.authentication(authenticationContainer -> authenticationContainer
-            .add(new DefaultBasicAuthentication("basic")));
-      }
-    }
   }
 
-  // This is a shim to work around an incompatible API change in Gradle 6.6. Prior to that,
-  // AbstractAuthenticationSupportedRepository#getConfiguredCredentials() returned a (possibly null)
-  // Credentials object. In 6.6, it changed to return Property<Credentials>.
-  //
-  // Compiling this plugin against Gradle 6.5 results in a NoSuchMethodException if you run it under
-  // Gradle 6.6. The same thing happens if you compile against 6.6 and run it in 6.5.
-  //
-  // So we have to use reflection to inspect the return type.
-  private static boolean shouldStoreCredentials(DefaultMavenArtifactRepository repo) {
+    private ArtifactRegistryPasswordCredentials getArtifactRegistryPasswordCredentials(ProviderFactory providerFactory, ArtifactRegistryPasswordCredentials crd) throws IOException {
+        CommandExecutor commandExecutor = new ProviderFactoryCommandExecutor(providerFactory);
 
-    try {
-      Method getConfiguredCredentials = DefaultMavenArtifactRepository.class
-          .getMethod("getConfiguredCredentials");
-
-      // This is for Gradle < 6.6. Once we no longer support versions of Gradle before 6.6
-      if (getConfiguredCredentials.getReturnType().equals(Credentials.class)) {
-        Credentials existingCredentials = (Credentials) getConfiguredCredentials.invoke(repo);
-        return existingCredentials == null;
-      } else if (getConfiguredCredentials.getReturnType().equals(Property.class)) {
-        Property<?> existingCredentials = (Property<?>) getConfiguredCredentials.invoke(repo);
-        return !existingCredentials.isPresent();
-      } else {
-        logger.warn("Error determining Gradle credentials API; expect authentication errors");
-        return false;
-      }
-    } catch (ReflectiveOperationException e) {
-      logger.warn("Error determining Gradle credentials API; expect authentication errors", e);
-      return false;
+        GoogleCredentials credentials = (GoogleCredentials) credentialProvider.getCredential(commandExecutor);
+        credentials.refreshIfExpired();
+        AccessToken accessToken = credentials.getAccessToken();
+        String token = accessToken.getTokenValue();
+        crd = new ArtifactRegistryPasswordCredentials("oauth2accesstoken", token, commandExecutor);
+        return crd;
     }
-  }
+
+    // The plugin for Gradle will apply Artifact Registry repo settings inside settings.gradle and build.gradle.
+    private void applyGradle(Gradle gradle, @Nullable ArtifactRegistryPasswordCredentials crd) {
+        gradle.settingsEvaluated(s -> modifySettings(s, crd));
+        gradle.projectsLoaded(g -> g.allprojects(p -> modifyProjectBuildscript(p, crd)));
+        gradle.projectsEvaluated(g -> g.allprojects(p -> modifyProject(p, crd)));
+    }
+
+    // The plugin for settings will apply Artifact Registry repo settings inside settings.gradle and build.gradle.
+    private void applySettings(Settings settings, @Nullable ArtifactRegistryPasswordCredentials crd) {
+        applyGradle(settings.getGradle(), crd);
+    }
+
+    // The plugin for projects will only apply Artifact Registry repo settings inside build.gradle.
+    private void applyProject(Project project, @Nullable ArtifactRegistryPasswordCredentials crd) {
+        project.afterEvaluate(p -> modifyProject(p, crd));
+    }
+
+    private void modifyProject(Project p, @Nullable ArtifactRegistryPasswordCredentials crd) {
+        p.getRepositories().forEach(r -> configureArtifactRegistryRepository(r, crd));
+        final PublishingExtension publishingExtension = p.getExtensions().findByType(PublishingExtension.class);
+        if (publishingExtension != null) {
+            publishingExtension.getRepositories().forEach(r -> configureArtifactRegistryRepository(r, crd));
+        }
+    }
+
+    // Not sure this knows which repositories already exist, so register a callback to modify the repos
+    // as they are added to the buildscript
+    private void modifyProjectBuildscript(Project p, @Nullable ArtifactRegistryPasswordCredentials crd) {
+        final ScriptHandler buildscript = p.getBuildscript();
+        if (buildscript != null) {
+            buildscript.getRepositories().whenObjectAdded(r -> configureArtifactRegistryRepository(r, crd));
+        }
+    }
+
+    private void modifySettings(Settings s, @Nullable ArtifactRegistryPasswordCredentials crd) {
+        s.getPluginManagement().getRepositories().forEach(r -> configureArtifactRegistryRepository(r, crd));
+        s.getDependencyResolutionManagement().getRepositories().forEach(r -> configureArtifactRegistryRepository(r, crd));
+    }
+
+    private void configureArtifactRegistryRepository(
+            ArtifactRepository repo, @Nullable ArtifactRegistryPasswordCredentials crd)
+            throws ProjectConfigurationException, UncheckedIOException {
+        if (!(repo instanceof DefaultMavenArtifactRepository)) {
+            return;
+        }
+        final DefaultMavenArtifactRepository arRepo = (DefaultMavenArtifactRepository) repo;
+        final URI u = arRepo.getUrl();
+        logger.error("Configuring repository" + u.toString());
+        if (u != null && u.getScheme() != null && u.getScheme().equals("artifactregistry")) {
+            try {
+                arRepo.setUrl(new URI("https", u.getHost(), u.getPath(), u.getFragment()));
+            } catch (URISyntaxException e) {
+                throw new ProjectConfigurationException(
+                        String.format("Invalid repository URL %s", u.toString()), e);
+            }
+
+            if (crd != null && shouldStoreCredentials(arRepo)) {
+                arRepo.setConfiguredCredentials(crd);
+                arRepo.authentication(authenticationContainer -> authenticationContainer
+                        .add(new DefaultBasicAuthentication("basic")));
+            }
+        }
+    }
+
+    // This is a shim to work around an incompatible API change in Gradle 6.6. Prior to that,
+    // AbstractAuthenticationSupportedRepository#getConfiguredCredentials() returned a (possibly null)
+    // Credentials object. In 6.6, it changed to return Property<Credentials>.
+    //
+    // Compiling this plugin against Gradle 6.5 results in a NoSuchMethodException if you run it under
+    // Gradle 6.6. The same thing happens if you compile against 6.6 and run it in 6.5.
+    //
+    // So we have to use reflection to inspect the return type.
+    private static boolean shouldStoreCredentials(DefaultMavenArtifactRepository repo) {
+
+        try {
+            Method getConfiguredCredentials = DefaultMavenArtifactRepository.class
+                    .getMethod("getConfiguredCredentials");
+
+            // This is for Gradle < 6.6. Once we no longer support versions of Gradle before 6.6
+            if (getConfiguredCredentials.getReturnType().equals(Credentials.class)) {
+                Credentials existingCredentials = (Credentials) getConfiguredCredentials.invoke(repo);
+                return existingCredentials == null;
+            } else if (getConfiguredCredentials.getReturnType().equals(Property.class)) {
+                Property<?> existingCredentials = (Property<?>) getConfiguredCredentials.invoke(repo);
+                return !existingCredentials.isPresent();
+            } else {
+                logger.warn("Error determining Gradle credentials API; expect authentication errors");
+                return false;
+            }
+        } catch (ReflectiveOperationException e) {
+            logger.warn("Error determining Gradle credentials API; expect authentication errors", e);
+            return false;
+        }
+    }
 }
